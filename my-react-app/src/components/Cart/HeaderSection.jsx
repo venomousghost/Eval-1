74ae5640a23c04d944c8f84483eef4064c72c066
navjot0079 <157043540+navjot0079@users.noreply.github.com>
2024-11-11 23:12:22 +0530

    updated header

--- my-react-app/src/components/Cart/HeaderSection.jsx
+++ my-react-app/src/components/Cart/HeaderSection.jsx
@@ -3,23 +3,25 @@ function HeaderSection(){
     return(
         <header>
             <nav>
-        <a href="#" className="logo">
-        <box-icon name='game' type='solid' color='#00FFFF' ></box-icon> Gaming<span>Sphere</span>
+        <a href="#" class="logo">
+            <box-icon name='game' type='solid' color='#00FFFF' ></box-icon>Gaming<span>UI</span>
         </a>
-        <ul className="navigation">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#cart">Cart</a></li>
-          <li><a href="#store">Store</a></li>
-          <li><a href="#login">Sign up/Sign in</a></li>
-          <li className="dropdown">
-            <a href="#more">More</a>
-            <ul className="dropdown-menu">
-              <li><a href="#about">About us</a></li>
-              <li><a href="#contact">Contact us</a></li>
-              <li><a href="#library">Library</a></li>
-              <li><a href="#tournament">Tournament</a></li>
-            </ul>
-          </li>
+        <div class="menuToggle" onclick="toggleMenu()">
+        </div>
+        <ul class="navigation">
+            <li><a href="home.html" onclick="toggleMenu()">Home</a></li>
+            <li><a href="/Cart" onclick="toggleMenu()">Cart</a></li>
+            <li><a href="/Profile" onclick="toggleMenu()">Profile</a></li>
+            <li><a href="/Login" onclick="toggleMenu()">Sign up/sign in</a></li>
+            <li class="dropdown">
+                <a href="#more" onclick="toggleMenu()">More</a>
+                <ul class="dropdown-menu">
+                    <li><a href="#option1" onclick="toggleMenu()">About us</a></li>
+                    <li><a href="/Contact" onclick="toggleMenu()">Contact us</a></li>
+                     <li><a href="/Library" onclick="toggleMenu()">Library</a></li>
+                </ul>
+            </li>
+
         </ul>
       </nav>
         </header>

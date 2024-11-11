7d57bad64615842839fa54c45b8608f62a9f15a5
navjot0079 <157043540+navjot0079@users.noreply.github.com>
2024-11-11 23:13:40 +0530

    contact updated

--- my-react-app/src/components/Contact/Contact.jsx
+++ my-react-app/src/components/Contact/Contact.jsx
@@ -45,25 +45,26 @@ const ContactUs = () => {
 
       <section className='contactx'>
         <nav>
-          <a href="#" className="logo">
-            <i className="fa fa-gamepad" style={{ color: '#00FFFF' }}></i> Gaming<span>sphere</span>
-          </a>
-          <div className="menuToggle" onClick={() => toggleMenu()}></div>
-          <ul className="navigation">
-            <li><a href="home.html">Home</a></li>
-            <li><a href="cart.html">Cart</a></li>
-            <li><a href="store.html">Store</a></li>
-            <li><a href="login.html">Sign up/Sign in</a></li>
-            <li className="dropdown">
-              <a href="#more">More</a>
-              <ul className="dropdown-menu">
-                <li><a href="about us page.html">About us</a></li>
-                <li><a href="contact.html">Contact us</a></li>
-                <li><a href="library.html">Library</a></li>
-                <li><a href="Tournament/index.html">Tournament</a></li>
-              </ul>
+          <a href="#" class="logo">
+            <box-icon name='game' type='solid' color='#00FFFF' ></box-icon>Gaming<span>UI</span>
+        </a>
+        <div class="menuToggle" onclick="toggleMenu()">
+        </div>
+        <ul class="navigation">
+            <li><a href="home.html" onclick="toggleMenu()">Home</a></li>
+            <li><a href="/Cart" onclick="toggleMenu()">Cart</a></li>
+            <li><a href="/Profile" onclick="toggleMenu()">Profile</a></li>
+            <li><a href="/Login" onclick="toggleMenu()">Sign up/sign in</a></li>
+            <li class="dropdown">
+                <a href="#more" onclick="toggleMenu()">More</a>
+                <ul class="dropdown-menu">
+                    <li><a href="#option1" onclick="toggleMenu()">About us</a></li>
+                    <li><a href="/Contact" onclick="toggleMenu()">Contact us</a></li>
+                     <li><a href="/Library" onclick="toggleMenu()">Library</a></li>
+                </ul>
             </li>
-          </ul>
+
+        </ul>
         </nav>
 
         <div className="containerx">

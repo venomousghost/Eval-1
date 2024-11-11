b9281696cba8c6fb63f222bcb09ba2f3afc64fe9
Manan <2005manangupta@gmail.com>
2024-11-11 22:59:57 +0530

    react update

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,10 @@
 import Mainpage from "./components/mainpage/mainpage";
 import {createBrowserRouter,RouterProvider} from "react-router-dom";
 import Login from "./components/login/Login";
+import Cart from "./components/cart/Cart";
+import Contact from "./components/Contact/Contact"
+import Library from "./components/Library/Library"
+// import ProfilePage from "./components/ProfilePage/ProfilePage";
 function App(){
   const router = createBrowserRouter([
     {
@@ -11,6 +15,22 @@ function App(){
       path: '/Login',
       element:<Login/>
     },
+    {
+      path: '/Cart',
+      element:<Cart/>
+    },
+    {
+      path: '/Contact',
+      element:<Contact/>
+    },
+    {
+      path: '/Library',
+      element:<Library/>
+    },
+    // {
+    //   path: '/Profile',
+    //   element:<ProfilePage/>
+    // },
   ]);
   return(
     <RouterProvider router={router}/>
--- my-react-app/src/components/mainpage/Navbar.jsx
+++ my-react-app/src/components/mainpage/Navbar.jsx
@@ -10,15 +10,15 @@ function Navbar() {
         </div>
         <ul class="navigation">
             <li><a href="home.html" onclick="toggleMenu()">Home</a></li>
-            <li><a href="cart.html" onclick="toggleMenu()">Cart</a></li>
-            <li><a href="store.html" onclick="toggleMenu()">Store</a></li>
+            <li><a href="/Cart" onclick="toggleMenu()">Cart</a></li>
+            <li><a href="/Profile" onclick="toggleMenu()">Profile</a></li>
             <li><a href="/Login" onclick="toggleMenu()">Sign up/sign in</a></li>
             <li class="dropdown">
                 <a href="#more" onclick="toggleMenu()">More</a>
                 <ul class="dropdown-menu">
                     <li><a href="#option1" onclick="toggleMenu()">About us</a></li>
-                    <li><a href="#option2" onclick="toggleMenu()">Contact us</a></li>
-                     <li><a href="#option3" onclick="toggleMenu()">Library</a></li>
+                    <li><a href="/Contact" onclick="toggleMenu()">Contact us</a></li>
+                     <li><a href="/Library" onclick="toggleMenu()">Library</a></li>
                 </ul>
             </li>
 

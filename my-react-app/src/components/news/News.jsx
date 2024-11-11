c19f8e7cd4419e954bf843f127986060c574d7e6
Mayank <singalmayank0000@gmail.com>
2024-11-11 22:45:32 +0530

    added

---
+++ my-react-app/src/components/news/News.jsx
@@ -0,0 +1,216 @@
+import React from 'react';
+import "./News.css"
+
+const News = () => {
+    return (
+        <div className="main">
+            <div className="main-inner">
+                <div className="main-wrap">
+                    <div className="news-left">
+                        {/* Featured Section */}
+                        <div className="featured">
+                            <div className="featured-wrap">
+                                <div className="left">Featured Games</div>
+                                <div className="right"><i className="fas fa-chevron-right"></i></div>
+                            </div>
+          +              </div>
+                        {/* News Section 1 */}
+                        <div className="news1">
+                            <div className="news1-wrap">
+                                <div className="news1-left">
+                                    <div className="news1-left-wrap">
+                                        <div className="date">
+                                            <i className="far fa-calendar-alt"></i> February 24, 2022
+                                        </div>
+                                        <h3>A lot of Happy this week aunday software a lot of edition</h3>
+                                        <div className="icons">
+                                            <div className="icon1"><i className="far fa-user fa-xl"></i> &nbsp;by <b>admin</b></div>
+                                            <div className="icon2"><i className="far fa-comments fa-lg"></i>&nbsp;&nbsp;&nbsp;0 Comments</div>
+                                            <div className="icon3"><i className="far fa-clock fa-lg"></i>&nbsp;&nbsp;3 minutes read</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="news1-right">
+                                    <div className="div">
+                                        <div className="left">
+                                            <img src="Tournament/4-420x420.jpg" alt="" />
+                                        </div>
+                                        <div className="right">
+                                            <div className="date">
+                                                <i className="far fa-calendar-alt"></i> February 24, 2022
+                                            </div>
+                                            <h3>Action dummy text of the printing type</h3>
+                                        </div>
+                                    </div>
+                                    <div className="div">
+                                        <div className="left">
+                                            <img src="Tournament/1-420x420.jpg" alt="" />
+                                        </div>
+                                        <div className="right">
+                                            <div className="date">
+                                                <i className="far fa-calendar-alt"></i> February 24, 2022
+                                            </div>
+                                            <h3>Computer is a soft market simply duty</h3>
+                                        </div>
+                                    </div>
+                                    <div className="div">
+                                        <div className="left">
+                                            <img src="Tournament/14-420x420.jpg" alt="" />
+                                        </div>
+                                        <div className="right">
+                                            <div className="date">
+                                                <i className="far fa-calendar-alt"></i> February 24, 2022
+                                            </div>
+                                            <h3>Everything You Need To Know About The</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* News Section 2 */}
+                        <div className="news2">
+                            <div className="news2-left">
+                                <div className="featured">
+                                    <div className="featured-wrap">
+                                        <div className="left">Featured Games</div>
+                                        <div className="right"><i className="fas fa-chevron-right"></i></div>
+                                    </div>
+                                </div>
+                                <div className="img">
+                                    <img src="Tournament/4-1020x600.jpg" alt="" />
+                                </div>
+                                <div className="date1">
+                                    <i className="far fa-calendar-alt"></i> February 24, 2022
+                                </div>
+                                <h2>Action dummy text of the printing type setting games 2022</h2>
+                                <p>WelcRimply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since.</p>
+                                <div className="icons">
+                                    <div className="icon1"><i className="far fa-user fa-xl"></i> &nbsp;by <b>admin</b></div>
+                                    <div className="icon2"><i className="far fa-comments fa-lg"></i>&nbsp;&nbsp;&nbsp;0 Comments</div>
+                                    <div className="icon3"><i className="far fa-clock fa-lg"></i>&nbsp;&nbsp;3 minutes read</div>
+                                </div>
+                                <div className="line"></div>
+                                <div className="div1">
+                                    <div className="div-right">
+                                        <div className="date">
+                                            <i className="far fa-calendar-alt"></i> February 24, 2022
+                                        </div>
+                                        <h4>Top 10 Best Computer games in the Marketing agency here 2018 games.</h4>
+                                    </div>
+                                </div>
+                                <div className="line"></div>
+                                <div className="div2">
+                                    <div className="div-right">
+                                        <div className="date">
+                                            <i className="far fa-calendar-alt"></i> February 24, 2022
+                                        </div>
+                                        <h4>Top 10 Best Computer games in the Marketing agency here 2018 games.</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="news2-right">
+                                <div className="featured">
+                                    <div className="featured-wrap">
+                                        <div className="left">Featured Games</div>
+                                        <div className="right"><i className="fas fa-chevron-right"></i></div>
+                                    </div>
+                                </div>
+                                <div className="img">
+                                    <img src="Tournament/3-1020x600.jpg" alt="" />
+                                </div>
+                                <div className="date1">
+                                    <i className="far fa-calendar-alt"></i> February 24, 2022
+                                </div>
+                                <h2>Top 10 Best Computer games in the Marketing agency here 2018 games.</h2>
+                                <p>WelcRimply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since.</p>
+                                <div className="icons">
+                                    <div className="icon1"><i className="far fa-user fa-xl"></i> &nbsp;by <b>admin</b></div>
+                                    <div className="icon2"><i className="far fa-comments fa-lg"></i>&nbsp;&nbsp;&nbsp;0 Comments</div>
+                                    <div className="icon3"><i className="far fa-clock fa-lg"></i>&nbsp;&nbsp;3 minutes read</div>
+                                </div>
+                                <div className="line"></div>
+                                <div className="div1">
+                                    <div className="div-right">
+                                        <div className="date">
+                                            <i className="far fa-calendar-alt"></i> February 24, 2022
+                                        </div>
+                                        <h4>Top 10 Best Computer games in the Marketing agency here 2018 games.</h4>
+                                    </div>
+                                </div>
+                                <div className="line"></div>
+                                <div className="div2">
+                                    <div className="div-right">
+                                        <div className="date">
+                                            <i className="far fa-calendar-alt"></i> February 24, 2022
+                                        </div>
+                                        <h4>Top 10 Best Computer games in the Marketing agency here 2018 games.</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* News Section 3 */}
+                        <div className="news3">
+                            <div className="grid gap-1 row-gap-1">
+                                <div className="g-col-4 grid1">
+                                    <div className="grid-wrap">
+                                        <div className="date">
+                                            <i className="far fa-calendar-alt"></i> May 31, 2022
+                                        </div>
+                                        <h3>Swiss Chard and Lamb Torte With Fennel Nation Pomegranate Relish</h3>
+                                    </div>
+                                </div>
+                                <div className="g-col-4 grid2">
+                                    <div className="grid-wrap">
+                                        <div className="date">
+                                            <i className="far fa-calendar-alt"></i> May 31, 2022
+                                        </div>
+                                        <h3>Swiss Chard and Lamb Torte With Fennel Nation Pomegranate Relish</h3>
+                                    </div>
+                                </div>
+                                <div className="g-col-4 grid3">
+                                    <div className="grid-wrap">
+                                        <div className="date">
+                                            <i className="far fa-calendar-alt"></i> May 31, 2022
+                                        </div>
+                                        <h3>Swiss Chard and Lamb Torte With Fennel Nation Pomegranate Relish</h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* News Section 4 */}
+                        <div className="news4">
+                            <div className="div">
+                                <div className="left">
+                                    <img src="Tournament/17-700x600.jpg" alt="" width="367px" height="314px" />
+                                </div>
+                                <div className="news4-right">
+                                    <div className="btn1">Racing</div>
+                                    <div className="icons">
+                                        <div className="icon1"><i className="far fa-calendar-alt"></i>&nbsp;May 31, 2022</div>
+                                        <div className="icon1"><i className="far fa-user fa-xl"></i> &nbsp;by <b>admin</b></div>
+                                        <div className="icon2"><i className="far fa-comments fa-lg"></i>&nbsp;&nbsp;&nbsp;0 Comments</div>
+                                        <div className="icon3"><i className="far fa-clock fa-lg"></i>&nbsp;&nbsp;3 minutes read</div>
+                                    </div>
+                                    <h4>Swiss Chard and Lamb Torte With Fennel Nation Pomegranate Relish</h4>
+                                    <p>WelcRimply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since.</p>
+                                    <div className="btn2">Read More</div>
+                                </div>
+                            </div>
+                            {/* Repeat similar blocks for other news articles */}
+                        </div>
+                    </div>
+                    <div className="news-right">
+                        <div className="news-right-add1">
+                            <img src="https://assets.xboxservices.com/assets/36/b5/36b52fa8-e71b-4435-888a-cecb98d3876a.jpg" alt="" />
+                        </div>
+                        <div className="news-right-add2">
+                            <img src="https://assets-prd.ignimgs.com/2024/08/18/blackmyth-1723969364570.jpg" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default News;

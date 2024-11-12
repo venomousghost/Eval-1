340d36581d94f9741b425b0a0545ffc915151705
navjot0079 <157043540+navjot0079@users.noreply.github.com>
2024-11-12 22:36:26 +0530

    react updated

--- my-react-app/src/components/Contact/Contact.jsx
+++ my-react-app/src/components/Contact/Contact.jsx
@@ -45,26 +45,25 @@ const ContactUs = () => {
 
       <section className='contactx'>
         <nav>
-          <a href="#" class="logo">
-            <box-icon name='game' type='solid' color='#00FFFF' ></box-icon>Gaming<span>UI</span>
-        </a>
-        <div class="menuToggle" onclick="toggleMenu()">
-        </div>
-        <ul class="navigation">
-            <li><a href="home.html" onclick="toggleMenu()">Home</a></li>
-            <li><a href="/Cart" onclick="toggleMenu()">Cart</a></li>
-            <li><a href="/Profile" onclick="toggleMenu()">Profile</a></li>
-            <li><a href="/Login" onclick="toggleMenu()">Sign up/sign in</a></li>
-            <li class="dropdown">
-                <a href="#more" onclick="toggleMenu()">More</a>
-                <ul class="dropdown-menu">
-                    <li><a href="#option1" onclick="toggleMenu()">About us</a></li>
-                    <li><a href="/Contact" onclick="toggleMenu()">Contact us</a></li>
-                     <li><a href="/Library" onclick="toggleMenu()">Library</a></li>
-                </ul>
+          <a href="#" className="logo">
+            <i className="fa fa-gamepad" style={{ color: '#00FFFF' }}></i> Gaming<span>sphere</span>
+          </a>
+          <div className="menuToggle" onClick={() => toggleMenu()}></div>
+          <ul className="navigation">
+            <li><a href="home.html">Home</a></li>
+            <li><a href="cart.html">Cart</a></li>
+            <li><a href="store.html">Store</a></li>
+            <li><a href="login.html">Sign up/Sign in</a></li>
+            <li className="dropdown">
+              <a href="#more">More</a>
+              <ul className="dropdown-menu">
+                <li><a href="about us page.html">About us</a></li>
+                <li><a href="contact.html">Contact us</a></li>
+                <li><a href="library.html">Library</a></li>
+                <li><a href="Tournament/index.html">Tournament</a></li>
+              </ul>
             </li>
-
-        </ul>
+          </ul>
         </nav>
 
         <div className="containerx">
@@ -82,6 +81,7 @@ const ContactUs = () => {
                     type="text"
                     id="fullname"
                     placeholder="Full name"
+                    className='blue'
                     value={formData.fullname}
                     onChange={handleChange}
                     required
@@ -89,6 +89,7 @@ const ContactUs = () => {
                   <input
                     type="email"
                     id="email"
+                    className='blue'
                     placeholder="E-mail"
                     value={formData.email}
                     onChange={handleChange}
@@ -97,6 +98,7 @@ const ContactUs = () => {
                   <input
                     type="text"
                     id="message"
+                    className='blue'
                     placeholder="Message"
                     value={formData.message}
                     onChange={handleChange}
